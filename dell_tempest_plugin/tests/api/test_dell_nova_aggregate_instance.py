6572d1542a7092518ac891d27cc9678ff5133cd5
Kumar Abhinav <kabhinav@gmail.com>
2017-02-02 23:13:40 +0000

    Knowns good state with names changed to dell

---
+++ dell_tempest_plugin/tests/api/test_dell_nova_aggregate_instance.py
@@ -0,0 +1,17 @@
+from dell_tempest_plugin.tests.api import base
+from tempest import test
+
+
+class TestMyHello(base.BaseHelloWorldTest):
+
+    @classmethod
+    def resource_setup(cls):
+        super(TestMyHello, cls).resource_setup()
+
+    @test.attr(type="smoke")
+    def test_nova_aggreagate_zone_instance_creation(self):
+        self.assertEqual('Hello world!', 'Hello world!')
+
+    @classmethod
+    def resource_cleanup(cls):
+        super(TestMyHello, cls).resource_cleanup()
