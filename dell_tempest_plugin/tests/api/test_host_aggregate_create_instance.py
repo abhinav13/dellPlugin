075742458c7ee4a4f31ef68a0fa8bc23585468b1
Kumar Abhinav <kabhinav@gmail.com>
2017-02-06 21:24:53 +0000

    Succefully add host to aggreagte zone

--- dell_tempest_plugin/tests/api/test_host_aggregate_create_instance.py
+++ dell_tempest_plugin/tests/api/test_host_aggregate_create_instance.py
@@ -23,23 +23,41 @@ class TestHostAggreagateCreateInstance(base.BaseDellTempestTestCase):
                                        uri=self.os_auth_url, cli_dir='/bin/')
 
     def create_host_aggregate(self, name="", availabilityzone=""):
-
         if not name:
-            name = 'AggName'
+            name = 'AggName' + str(self.getUniqueInteger())
         if not availabilityzone:
-            availabilityzone = 'AvZ'
+            availabilityzone = 'AvZ' + str(self.getUniqueInteger())
         raw_output = self.cliclient.nova(action='aggregate-create', params=name + ' ' + availabilityzone)
         LOG.info("create_host_aggregate() raw output %s", raw_output)
         new_zone = output_parser.listing(raw_output)
         self.assertNotEqual(len(new_zone), 0)
         return new_zone[0]
 
+    def get_compute_host_details(self):
+        raw_output = self.cliclient.nova(action='host-list')
+        all_hosts = output_parser.listing(raw_output)
+        LOG.info("get_compute_host_details raw output %s", raw_output)
+        LOG.info("get_compute_host_details listing %s type %s", str(all_hosts),str(type(all_hosts)))
+
+        self.assertNotEmpty(all_hosts)
+        #Filter the host list for compute nodes only
+        compute_hosts = list(filter(lambda x: str(x['service']).lower() == 'compute', all_hosts))
+        LOG.info("compute_hosts %s", str(compute_hosts))
+        self.assertNotEmpty(compute_hosts)
+        return compute_hosts[0]
+
+    def add_compute_host_to_aggregate_zone(self,agg_id,host_name):
+        ret_val= self.cliclient.nova(action='aggregate-add-host', params=agg_id + ' ' + host_name)
+        LOG.info("return value of adding host  %s", ret_val)
+
     @test.attr(type="dell")
     def test_nova_host_aggregate_instancecreation(self):
-
         LOG.info("BEGIN: test_nova_host_aggregate_instancecreation")
         ag_zone = self.create_host_aggregate()
         LOG.info("ag_zone id %s", ag_zone['Id'])
+        host_details = self.get_compute_host_details()
+        LOG.info("Host name where will create instances %s", str(host_details['host_name']))
+        self.add_compute_host_to_aggregate_zone(ag_zone['Id'], host_details['host_name'])
         self.assertEqual('Hello world!', 'Hello world!')
 
     @classmethod
